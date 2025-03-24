b43074e51423d136069e7ff1a8f3e55a6a41dc06
Yen-san <ebyenmadrid@gmail.com>
2025-03-24 17:24:21 +0800

    separated header and footer including styles, added javascript to retrieve header and footer

---
+++ admission-main/components/script-head-foot.js
@@ -0,0 +1,14 @@
+function loadHTML(id, file) {
+    fetch(file)
+        .then(response => {
+            if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+            return response.text();
+        })
+        .then(data => document.getElementById(id).innerHTML = data)
+        .catch(error => console.error(`Error loading ${file}:`, error));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadHTML("header", "../components/header.html");            
+    loadHTML("footer", "../components/footer.html"); 
+});
